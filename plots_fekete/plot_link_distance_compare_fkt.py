3d53cadafba9d1c76ed5cb4ed66a2d1c5bd22ffe
Hem-W <hm_wang@outlook.com>
2025-08-29 17:34:59 +0800

    refactor: removing unused plotting function and updating axis label

--- plots_fekete/plot_link_distance_compare_fkt.py
+++ plots_fekete/plot_link_distance_compare_fkt.py
@@ -9,48 +9,6 @@ import matplotlib.pyplot as plt
 mpl.rcParams['font.sans-serif'] = "Myriad Pro"
 mpl.rcParams['font.size'] = 9
 
-
-def plot_loghist(dist, cos_lat, title, figname, sample=None):
-    angdist = dist.data
-    angdist = angdist[angdist > 0]
-
-    tic = time.time()
-
-    logbins = np.logspace(np.log10(1), np.log10(20015), 50)
-    if sample is None:
-        dist_sam = angdist
-    else:
-        dist_sam = np.random.choice(angdist, size=sample, replace=False)
-    loghist = np.histogram(dist_sam, bins=logbins, density=True)
-    print("Histogram Ended: {:.2f}s".format(time.time() - tic))
-
-    x_min, x_max = 500, 2500
-    x_cut = loghist[1][(loghist[1] > x_min) & (loghist[1] < x_max)]  # fit power-law # TODO: 这里xy对嘛？
-    y_cut = loghist[0][(loghist[1][:-1] > x_min) & (loghist[1][:-1] < x_max)]  # 实测 histogram
-    plfit = lambda params: np.sum(np.abs(params[0] * x_cut ** (-params[1]) - y_cut))  # a*x_cut^(-b) - y_cut
-    pl_params = so.fmin(plfit, [1, 1], disp=False)
-    logx = loghist[1][:-1] + (loghist[1][1:] - loghist[1][:-1]) / 2.0
-    x_prd = logx[logx > x_min]
-
-    fig = plt.figure(figsize=(6, 4), tight_layout=True)
-    ax1 = fig.add_subplot(111)
-    ax1.loglog(logx[logx <= 2500], loghist[0][logx <= 2500], color='r', ls='None', marker='o',
-               fillstyle='none', label=r'No weights ($d\leq$ 2500km)')
-    ax1.loglog(logx[logx > 2500], loghist[0][logx > 2500], color='b', ls='None', marker='o',
-               fillstyle='none', label=r'No weights ($d>$ 2500km)')
-    # ax1.loglog(logx[logx <= 2500], loghist_weight[0][logx <= 2500], color='r', ls='None', marker='o',
-    #            fillstyle='full', label=r'Weighted ($d\leq$ 2500km)')
-    # ax1.loglog(logx[logx > 2500], loghist_weight[0][logx > 2500], color='b', ls='None', marker='o',
-    #            fillstyle='full', label=r'Weighted ($d>$ 2500km)')
-    ax1.loglog(x_prd, pl_params[0] * x_prd ** (-pl_params[1]), '--', alpha=0.5, lw=1.5, color='r',
-               label=r'Powerlaw fit, $\alpha$ = %.3f' % (pl_params[1]))
-    ax1.axvline(x=2500, color='m', alpha=.6)
-    ax1.set(xlabel='Distance [km]', ylabel='PDF', title=title)
-    ax1.legend(loc="lower right")
-    fig.savefig(figname, dpi=300, bbox_inches='tight')
-    fig.show()
-    return None
-
 # %%
 def output_figcase(figcase):
     datanm = "spimv2fkt"
@@ -123,7 +81,7 @@ def output_figcase(figcase):
                 label="Distance=2500 km")
     ax1.set_xlim(50, 30000)
     ax1.set_ylim(10**-5 if density else 10**4)
-    ax1.set(xlabel='Distance [km]', ylabel='Probability [-]' if density else "Frequency of link distances [-]",
+    ax1.set(xlabel='Distance [km]', ylabel='Probability [-]' if density else "Total frequency of link distances [-]",
             title=title)
     ax1.set(xticks=[100, 1000, 10000])
     ax1.set(xticklabels=["$\\mathdefault{100}$", "$\\mathdefault{1000}$", "$\\mathdefault{10000}$"])
